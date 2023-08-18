c597a251d8c248c2b44782041ed6b5ba6768f865
mikey <mikey@hey.com>
2023-08-17 20:38:36 -0500

    add example 5 solution

--- lesson-1/example-5.py
+++ lesson-1/example-5.py
@@ -6,10 +6,20 @@ high_score = 60
 
 # Let's use variable reassignment to update the scores!
 my_score = my_friend_score
+# my_score = 50
 my_friend_score = high_score
+# my_friend_score = 60
 my_score = 55
+# my_score = 55
 high_score = 99
+# high_score = 99
 my_score = high_score
+# my_score = 99
 
 # Draw a variable table to visualize how these variables' values are changing!
 # Use a block comment.
+'''
+my_score =  99
+my_friend_score = 60
+high_score = 99
+'''
