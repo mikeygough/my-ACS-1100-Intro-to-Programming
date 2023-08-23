53e8fecd6d8744db20b2cc35af27188ceba529a7
mikey <mikey@hey.com>
2023-08-23 15:47:46 -0500

    add completed lesson-3 examples

--- lesson-3/example-3.py
+++ lesson-3/example-3.py
@@ -1,28 +1,28 @@
 # Here is the double function
 
 def double(number):
-	# double number and 
-	return 0
+	# double number and return it 
+	return number * 2
 
 first_number = double(100)
 second_number = double(25)
 
-print(a) # 200
-print(b) # 50
+print(first_number) # 200
+print(second_number) # 50
 
 #TODO: Cuber. This function should multiply three numbers and return the answer
 
 def cuber(a, b, c): 
-	return 0
+	return a * b * c
 
-cuber(3, 3, 3) # Should return 27
+third_number = cuber(3, 3, 3) # Should return 27
+print(third_number)
 
 #TODO: Write a function to generate product
 
 def product(a, b):
 	# product is a * b
-	return 0
+	return a * b
 
 # double 3 and cube 2 and return the product
-
-
+print(product(double(3), cuber(2, 2, 2)))
