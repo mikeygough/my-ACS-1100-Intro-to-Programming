0c3d89d929487a1769e2c15d87eb364c4654f199
mikey <mikey@hey.com>
2023-08-23 15:41:22 -0500

    add function to custom-calc

--- lesson-2/custom-calc.py
+++ lesson-2/custom-calc.py
@@ -23,6 +23,8 @@
 
 # Be sure to convert your numeric values to numbers before performing math operations!
 
+''' !! non-function version !! '''
+
 import datetime
 
 print("\nWecome to the Days-Between Calculator!")
@@ -60,6 +62,45 @@ day = int(end_date[8:10])
 
 # create datetime object
 end_date = datetime.datetime(year, month, day)
+
+# calculate days_between
+days_between = abs((start_date - end_date).days)
+
+# print result
+print(f"Days Between: {days_between:,}\n")
+
+
+''' !! function version !! '''
+import datetime
+
+print("\nWecome to the Days-Between Calculator!")
+print("The Days-Between Calculator calculates the absolute number of days between a start_date and end_date.")
+print("Dates should be entered in the format YYYY-MM-DD. For example, August 1, 2023 entered as 2023-08-01.\n")
+
+def get_date(user_date):
+    """''get_date() prompts a user for a date and returns a datetime object of that date (YYYY-MM-DD)
+        user_date: a string to format the input prompts. for example, user_date='start_date'"""
+    
+    # prompt user
+    d = input(f"What is the {user_date}? (In the format YYYY-MM-DD): ")
+
+    # error checking, ensure length matches YYYY-MM-DD
+    while len(str(d)) != 10:
+        print("Invalid Format! Please try again.")
+        d = input(f"What is the {user_date}? (In the format YYYY-MM-DD): ")
+
+    # select year, month and day. convert to int
+    year = int(d[0:4])
+    month = int(d[5:7])
+    day = int(d[8:10])
+
+    # create datetime object
+    d = datetime.datetime(year, month, day)
+
+    return d
+
+start_date = get_date("start_date")
+end_date = get_date("end date")
 
 # calculate days_between
 days_between = abs((start_date - end_date).days)
